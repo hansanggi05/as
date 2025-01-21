c1ac417fb676431606f99e8907ef86a97485d59c
hansanggi05 <hsk01087255071@gmail.com>
2025-01-21 11:47:18 +0900

    Add files via upload

---
+++ 숙제.cpp
@@ -0,0 +1,77 @@
+//#include <stdio.h>
+//int main()
+//{
+//	printf("C 언어\n");
+//	printf("200+300\n");
+//	printf("%d\n", 200 + 300);
+//
+//	printf("%d %d %d %d\n", 100, 200, 300);
+//	printf("%d %d\n",100,200, 300);
+//	printf("%d\n",100,200, 300);
+//
+//	printf("%d / %d = %d \n", 10, 20, 10 / 20);
+//	
+//	printf("%05d\n", 4321);
+//	printf("%7.1f\n", 4321.7890);
+//	printf("%5s\n", "Cook");
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int a, b, c, res;
+//	a= b= c = 10;
+//	res = a * b * c;
+//	printf("a,b,c 의 곱셈 값 == >%d \n", res);
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char ch1 = 'A', ch2 = 'B', ch3 = 'c';
+//	ch1 = ch2 = 100;
+//	ch1 = ch2 = 'T';
+//	ch1 = ch2 = 200;
+//	ch3 = ch1 - ch2;
+//	printf(ch3, ch1, ch2);
+//
+//
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	char str[10] = "VisualC++";
+//	int i;
+//	for()
+//
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	printf("f111\n");
+//	printf("111+222\n");
+//	printf("%d\n", 111 + 222);
+//
+//	float a = 123.45;
+//	printf("%08.3f\n", a);
+//
+//	
+//}
+
+#include <stdio.h>
+int main()
+{
+	float a = 123.45, b;
+	int c;
+	c = a;
+	b = c;
+	printf("%6.2f\n", b);
+
+	return 0;
+}
