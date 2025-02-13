f9a266295cd5b43506f49669fb00100e182cd8a9
hansanggi05 <hsk01087255071@gmail.com>
2025-02-13 16:53:34 +0900

    Add files via upload

---
+++ 소스0213.cpp
@@ -0,0 +1,55 @@
+//#include <stdio.h>
+//int main()
+//{
+//	int input1, input2;
+//	printf("시작 값 ==> ");
+//	scanf("%d", &input1);
+//	printf("끝 값 ==> ");
+//	scanf("%d", &input2);
+//	int s, e;
+//	if (input1 > input2)
+//	{
+//		s = input2;
+//		e = input1;
+//	}
+//	else if (input1 < input2)
+//	{
+//		s = input1;
+//		e = input2;
+//	}
+//	while (s <= e)
+//	{
+//		if (s % 2 == 1)
+//			printf("%d ", s);
+//		s++;
+//	}
+//	return 0;
+//}
+
+//#include <stdio.h>
+//int main()
+//{
+//	int i, k;
+//	i = 0;
+//	while (i < 3)
+//	{
+//		k = 0;
+//		while (k < 4)
+//		{
+//			printf("중첩 while문\n");
+//			k++;
+//		}
+//		i++;
+//	}
+//}
+
+
+#include <stdio.h>
+int main()
+{
+	int i;
+	printf("숫자를 여러 개 입력 : ");
+	scanf("%d", &i);
+	
+	printf("자리 수 ");
+}
